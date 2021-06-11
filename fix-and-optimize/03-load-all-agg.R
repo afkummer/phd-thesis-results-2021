7564bde97f7651672500fef74446cc807199b4df
alberto <alberto@inf.ufsm.br>
2021-06-11 13:47:38 -0300

    Add progress of solution plot script for F&O

---
+++ fix-and-optimize/03-load-all-agg.R
@@ -0,0 +1,42 @@
+#!/usr/bin/env Rscript
+
+require(dplyr)
+require(data.table)
+
+# First read all results for each one of the experiments to memory.
+all.rg <- fread("random-guided/all.fx.csv") %>% as_tibble() %>% mutate(exp="R+G")
+all.ro <- fread("random-only/all.fx.csv") %>% as_tibble() %>% mutate(exp="R")
+all.go <- fread("guided-only/all.fx.csv") %>% as_tibble() %>% mutate(exp="G")
+
+all <- rbind(all.rg, all.ro, all.go)
+all$exp <- as.factor(all$exp)
+
+plotInstanceProgress <- function(instance.name, iter.beg=0, iter.end=99999) {
+   cat("Printing progress plot for instance ", instance.name, "\n")
+   tmp <- all %>% 
+      dplyr::filter(instance == instance.name & iter >= iter.beg & iter < iter.end) %>%
+      group_by(iter, exp) %>%
+      mutate(
+         obj.mean = mean(obj),
+         obj.sd = sd(obj),
+         obj.min = min(obj), 
+         obj.max = max(obj)
+      ) %>%
+      ungroup() 
+
+   cat("Preprocessed data:\n")
+   print(head(tmp))
+
+   plt <- ggplot(tmp, aes(iter, obj.mean, color=exp, fill=exp)) + 
+      geom_line() + 
+      geom_ribbon(aes(ymin=obj.min, ymax=obj.max), alpha=0.05, linetype=3) +
+      geom_hline(yintercept=min(tmp$obj), linetype=2) +
+      theme_bw(16) + 
+      xlab("Iteration") +
+      ylab("Solution cost")
+
+   dev.new()
+   print(plt)
+   return(plt)
+}
+
