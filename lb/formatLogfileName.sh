e2ca6c5c0a60f2183fea259f5f68ab18f1ce1ba4
alberto <alberto@inf.ufsm.br>
2021-08-14 15:51:00 -0300

    Adds a shell script for automatic logfile renaming

--- lb/formatLogfileName.sh
+++ lb/formatLogfileName.sh
@@ -6,12 +6,12 @@ fi
 
 echo "Formatting name of '$1'"
 
-path="$(cat $1 | grep 'Instance: ' | cut -d ' ' -f 2)"
+path="$(cat $1 | grep 'Instance file: ' | cut -d ' ' -f 4)"
 file="$(basename $path .txt)"
 printf "   Instance path: %s\n" $path
 printf "   Basename: %s\n" $file
 
 newname="$(dirname $1)/${file}.log"
 echo "Moving $1 to $newname"
-mv $1 $newname
+git mv $1 $newname
 
