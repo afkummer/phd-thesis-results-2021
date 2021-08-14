bdbf95d4642dce5d56710934b715c76235c526ea
alberto <alberto@inf.ufsm.br>
2021-08-14 20:47:21 -0300

    Adds a log formatter script

---
+++ fix-and-optimize/scripts/formatLogfileName.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+if [ $# -ne 1 ]; then
+   exit 1
+fi
+
+echo "Formatting name of '$1'"
+
+path="$(xzcat $1 | grep 'Instance: ' | cut -d ' ' -f 3)"
+file="$(basename $path .txt)"
+printf "   Instance path: %s\n" $path
+printf "   Basename: %s\n" $file
+
+seed="$(xzcat $1 | grep 'Seed: ' | cut -d ' ' -f 3)"
+printf "   Seed: %s\n" $seed
+
+newname="${file}-${seed}.log.xz"
+echo "Moving $1 to $newname"
+#mv $1 $newname
+
