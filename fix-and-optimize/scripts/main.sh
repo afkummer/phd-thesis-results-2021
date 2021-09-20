80c7ceb4dac1b2e055ccdb34c35c30eb2ee80a8a
alberto <alberto@inf.ufsm.br>
2021-09-20 19:01:34 -0300

    Last-minute change in main.sh for F&O

--- fix-and-optimize/scripts/main.sh
+++ fix-and-optimize/scripts/main.sh
@@ -13,16 +13,16 @@ xzcat ../raw-csv/random-guided.csv.xz  > ../out/random-guided.csv
 xzcat ../raw-csv/random-only.csv.xz  > ../out/random-only.csv
 
 printf "\n(2/4) Fixing instance names...\n"
-Rscript ./01-fix_names.R ../out/guided-only.csv ../out/guided-only.fixed.csv
-Rscript ./01-fix_names.R ../out/random-guided.csv ../out/random-guided.fixed.csv
-Rscript ./01-fix_names.R ../out/random-only.csv ../out/random-only.fixed.csv
+Rscript ./01-fix-names.R ../out/guided-only.csv ../out/guided-only.fixed.csv
+Rscript ./01-fix-names.R ../out/random-guided.csv ../out/random-guided.fixed.csv
+Rscript ./01-fix-names.R ../out/random-only.csv ../out/random-only.fixed.csv
 
 printf "\n\n(3/4) Computing mean results...\n"
-Rscript ./02-generate_means.R ../out/guided-only.fixed.csv ../out/guided-only.means.csv 
-Rscript ./02-generate_means.R ../out/random-guided.fixed.csv ../out/random-guided.means.csv
-Rscript ./02-generate_means.R ../out/random-only.fixed.csv ../out/random-only.means.csv
+Rscript ./02-generate-means.R ../out/guided-only.fixed.csv ../out/guided-only.means.csv 
+Rscript ./02-generate-means.R ../out/random-guided.fixed.csv ../out/random-guided.means.csv
+Rscript ./02-generate-means.R ../out/random-only.fixed.csv ../out/random-only.means.csv
 
 printf "\n\n(4/4) Generating graphs comparing matheuristic variations...\n"
 Rscript ./03-algo-comparator.R ../out/random-guided.fixed.csv ../out/guided-only.fixed.csv ../out/random-only.fixed.csv
 
-printf "\n\nOutput data stored in ../out\n\n"
+printf "\n\nOutput data stored in $(realpath ../out)\n\n"
